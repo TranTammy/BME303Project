00fb9eaa93509ece1388942af80fedd9ceaf75db
Tammy Tran <93611667+TranTammy@users.noreply.github.com>
2022-11-28 20:27:32 -0600

    Add files via upload

--- matrixs.py
+++ matrixs.py
@@ -9,40 +9,51 @@ from matplotlib import colors
 # You need to plot the 2D domain and the number of elements over the 100 iterations
 # 100 iterations (at least)
 
-# matrix states: empty(0), affected(1), recovery(2)
+# matrix states: asymptomatic(0), affected(1), recovery(2)
 """
-arr = np.random.random((30,30))
-print(arr)
+# update the states (etc asymptomatic, affected, recovery) based on neighborhood
+def updateStates(#code here):
+    #code here
 
-H = np.array([[1, 2, 3, 4],
-          [5, 6, 7, 8],
-          [9, 10, 11, 12],
-          [13, 14, 15, 16]])
-
-plt.imshow(arr, interpolation='none')
-plt.show()
+# update the position (aka the neighborhood and moving the recovery)
+def updatePositions(#code here):
+    #code here
 """
+
+# plots grid graph
 def plotSpatial(data, fileNum):
-    colormap = colors.ListedColormap(['gray','blue','red'])
+    colormap = colors.ListedColormap(['white','blue','red'])
     plt.figure(figsize=(7,6))
     plt.pcolor(data, cmap=colormap, edgecolors='k', linewidths=1, vmin=0,vmax=2)
     cbar = plt.colorbar(label="", orientation="vertical", ticks=[0.33,1,1.66])
     cbar.ax.set_yticklabels(['No symptoms', 'Affected', 'Recovering'])
     plt.imshow(data)
+    plt.show()
     plt.savefig('figure_' + str(fileNum) + '.jpg', bbox_inches='tight', pad_inches=0.02)
     plt.close()
-    plt.grid()
 
 
 def main():
+    # initial random matrix
     random.seed(time.time())
     currTime = 0
     sizeX, sizeY = 30, 30
     domain = np.array([random.randint(0, 2) for x in range(sizeX*sizeY)]).reshape(sizeY,sizeX)
     print(domain)
-    # plotSpatial(domain, currTime)
-    plt.imshow(domain, interpolation='none')
-    plt.show()
 
+    # plots initial matrix
+    plotSpatial(domain, currTime)
 
+    # adds values
+    simTime, asymptomatic, affected, recovery = [], [], [], []
+    simTime.append(currTime)
+    asymptomatic.append(np.count_nonzero(domain == 0))
+    affected.append(np.count_nonzero(domain == 1))
+    recovery.append(np.count_nonzero(domain ==2))
+
+    # for loop for 100 iterations
+    for currTime in range(1, 101):
+        print(currTime)
+    # code here
+    
 main()
